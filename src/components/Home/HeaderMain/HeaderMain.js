e5b466c69204ff0a51a623a5a69c9f486fcc3452
Farukhrana14 <farukhrana14@gmail.com>
2021-05-19 06:57:56 +0600

    readme updated

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -6,16 +6,17 @@ import chair from '../../../images/chair.png';
 
 const HeaderMain = () => {
     return (
-        <main style={{height: '600px'}} className='row d-flex align-items-center'>
+        <main style={{ height: '600px' }} className='row d-flex align-items-center'>
             <div className='col-md-4 col-sm-6 col-12 offset-md-1'>
-                <h1 style={{color: '#3A4256'}}>Your New Smile <br/> Starts Here</h1>
+                <h1 style={{ color: '#3A4256' }}>PearlSmile Dental Care</h1>
+                <h2 style={{ color: '#3A4256' }}>Your New Smile <br /> Starts Here</h2>
                 <p className='text-secondary'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque dolorem quia labore magnam rem architecto! </p>
                 <Link to="/appointment" className="btn btn-brand">GET APPOINTMENT</Link>
             </div>
-            
+
             <div className='col-md-6 col-sm-6 col-12'>
-                <img src={chair} alt="" className='img-fluid'/>
-            </div>            
+                <img src={chair} alt="" className='img-fluid' />
+            </div>
 
         </main>
     );
